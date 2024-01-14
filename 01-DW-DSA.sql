f0fedd34efd86ca0ddd9632da169415e2150f2af
micael.dias <micael.dias@uphold.com>
2024-01-14 20:04:26 +0000

    Add DataWarehouse tables

--- 01-DW-DSA.sql
+++ 01-DW-DSA.sql
@@ -1,2 +1,100 @@
-CREATE DATABASE DataStagingArea;
-CREATE DATABASE DataWarehouse;
+create database DataStagingArea
+go
+
+create database DataWarehouse
+go
+
+use DataWarehouse
+go
+
+create table DimTimestamp
+(
+    SurrogateKey  int        not null
+        constraint DimTimestamp_SurrogateKey
+            primary key,
+    UnixTimestamp bigint     not null,
+    HH            int        not null,
+    MM            int        not null,
+    SS            int        not null,
+    AM_PM         int        not null,
+    Day           int        not null,
+    Week          int        not null,
+    WeekName      varchar(9) not null,
+    Month         int        not null,
+    MonthName     varchar(9) not null,
+    Quarter       int        not null,
+    Year          int        not null
+)
+go
+
+create table DimBlock
+(
+    SurrogateKey     int       not null
+        constraint DimBlock_SurrogateKey
+            primary key,
+    BlockHash        varbinary not null,
+    GasLimit         bigint    not null,
+    GasUsed          bigint    not null,
+    TimestampKey     int       not null
+        constraint DimBlock_DimTimestamp_SurrogateKey_Foreign_Key
+            references DimTimestamp,
+    TransactionCount bigint    not null,
+    BaseFeePerGas    bigint    not null
+)
+go
+
+create table DimToken
+(
+    Address      varbinary   not null
+        constraint DimToken_Address
+            unique,
+    Symbol       varchar(4)  not null,
+    Name         varchar(50) not null,
+    Decimals     bigint      not null,
+    TotalSupply  int         not null,
+    ERC20        bit         not null,
+    ERC721       bit         not null,
+    SurrogateKey int         not null
+        constraint DimToken_SurrogateKey_Foreign_Key
+            primary key
+)
+go
+
+create table FactBalance
+(
+    SurrogateKey int       not null
+        constraint FactBalance_SurrogateKey
+            primary key,
+    Address      varbinary not null,
+    BalanceETBH  bigint    not null,
+    BalanceUSD   bigint    not null,
+    BalanceEUR   bigint    not null,
+    TimestampKey int       not null
+        constraint FactBalance_DimTimestamp_SurrogateKey_Foreign_Key
+            references DimTimestamp
+)
+go
+
+create table FactTransaction
+(
+    TransactionHash binary    not null,
+    BlockKey        int       not null
+        constraint FactTransaction_DimBlock_SurrogateKey_Foreign_Key
+            references DimBlock,
+    FromAddress     varbinary not null,
+    ToAddress       varbinary not null,
+    TokenKey        int       not null
+        constraint FactTransaction_DimToken_SurrogateKey_Foreign_Key
+            references DimToken,
+    ValueETH        bigint    not null,
+    ValueUSD        bigint    not null,
+    ValueEUR        bigint    not null,
+    GasUsed         bigint    not null,
+    GasPrice        bigint    not null,
+    GasPaid         bigint    not null,
+    TransactionType int       not null,
+    SurrogateKey    int       not null
+        constraint FactTransaction_SurrogateKey
+            primary key
+)
+go
